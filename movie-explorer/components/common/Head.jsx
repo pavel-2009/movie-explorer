3aca656f418e561482bd73016baa9e98983375ad
pavel-2009 <ms.radionova@bk.ru>
2026-07-11 15:22:48 +0500

    created component for head

---
+++ movie-explorer/components/common/Head.jsx
@@ -0,0 +1,12 @@
+function Head() {
+    return (
+        <head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Movie Explorer</title>
+            <link rel="preconnect" href="https://image.tmdb.org"/>
+            <link rel="preconnect" href="https://api.iconify.design"/>
+            <link rel="stylesheet" href="styles.css"/>
+        </head>
+    )
+}
