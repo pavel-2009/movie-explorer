232aeaf8e3b73a2b45bd2ee335cf3580d4dc0e80
pavel-2009 <ms.radionova@bk.ru>
2026-07-13 16:34:22 +0500

    need to fix search field

--- movie-explorer/src/components/layout/Header.jsx
+++ movie-explorer/src/components/layout/Header.jsx
@@ -13,6 +13,9 @@ export function Header() {
                     <span class="title__first">Movie</span>
                     <span class="title__second">Explorer</span>
                 </a>
+                <form action="/search" className="search-form">
+                    <input type="text" placeholder="Поиск фильмов..." className="search-form__input"/>
+                </form>
                 <button class="icon-button" aria-label="Поиск">
                     <svg class="icon-button__icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path d="M21 21L15.0001 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
