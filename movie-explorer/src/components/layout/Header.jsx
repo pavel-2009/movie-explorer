18ffe54552af9648c40d842ffdc499c53a4b9b0e
pavel-2009 <ms.radionova@bk.ru>
2026-07-16 09:12:12 +0500

    updated menu button on tablet

--- movie-explorer/src/components/layout/Header.jsx
+++ movie-explorer/src/components/layout/Header.jsx
@@ -7,7 +7,7 @@ export function Header() {
         <header className="app-header">
             <div className="head">
                 <div className="menu-button" aria-label="Меню">
-                    <svg className="menu" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="menu" width="27" height="27" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 12H21" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M3 6H21" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M3 18H21" stroke="#F9FAFB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
