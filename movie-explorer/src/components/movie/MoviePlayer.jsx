2c82efb929c8360b28a30dcb033a4af8385c486b
pavel-2009 <ms.radionova@bk.ru>
2026-07-16 21:38:14 +0500

    need to fix movie player

--- movie-explorer/src/components/movie/MoviePlayer.jsx
+++ movie-explorer/src/components/movie/MoviePlayer.jsx
@@ -18,35 +18,29 @@ export function MoviePlayer() {
   const movie = movies.find((item) => item.slug === slug) ?? defaultMovie
 
   return (
-    <main className="main-content movie-page movie-player" aria-labelledby="movie-player-title">
+    <main className="main-content movie-player" aria-labelledby="movie-player-title">
       <section className="movie-player__screen" aria-label={`Просмотр фильма ${movie.title}`}>
         <img className="movie-player__backdrop" src={movie.backdrop} alt={`Постер фильма ${movie.title}`} />
         <div className="movie-player__overlay" />
-        <div className="movie-player__content">
-          <span className="movie-player__badge">Сейчас смотрите</span>
-          <h1 className="movie-page__title movie-player__title" id="movie-player-title">
-            {movie.title}
-          </h1>
-          <div className="movie-page__meta movie-player__meta" aria-label="Информация о фильме">
-            <span className="movie-page__rating">
-              <img src={starIcon} alt="" />
-              {movie.rating}
-            </span>
-            <span>{movie.genre}</span>
-            <span>{movie.duration}</span>
-          </div>
-
-          <div className="movie-player__controls">
-            <button type="button" className="movie-page__watch movie-player__button">
-              <img src={playIcon} alt="" />
-              Воспроизвести
-            </button>
-            <Link to={`/movie/${slug}`} className="movie-player__link">
-              К описанию
-            </Link>
-          </div>
-        </div>
       </section>
+      <div className="movie-player__content">
+        <h1 className="movie-page__title movie-player__title" id="movie-player-title">
+          {movie.title}
+        </h1>
+        <div className="movie-page__meta movie-player__meta" aria-label="Информация о фильме">
+          <span>{movie.duration}</span>
+        </div>
+
+        <div className="movie-player__controls">
+          <button type="button" className="movie-page__watch movie-player__button">
+            <img src={playIcon} alt="" />
+            Воспроизвести
+          </button>
+          <Link to={`/movie/${slug}`} className="movie-player__link">
+            К описанию
+          </Link>
+        </div>
+      </div>
 
       <section className="movie-page__details movie-player__details" aria-label="Краткая информация о фильме">
         <div>
